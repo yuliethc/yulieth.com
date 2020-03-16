46f9de37cc392ef17e47a91f3ca491b62e617c03
yuliethc <yuliethdotcom@gmail.com>
2020-03-15 21:12:12 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-Here I'm creating my personal site for yulieth.com, a site where I can show my work and interests.
---
+++ precache-manifest.1b76fc217f9d8932e8183060e06b96a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c3491de380b961fde3998242338ba6",
+    "url": "/yulieth.com/index.html"
+  },
+  {
+    "revision": "b0848a58de121a63f288",
+    "url": "/yulieth.com/static/css/main.2454348c.chunk.css"
+  },
+  {
+    "revision": "adb5c822bdb35b6dda31",
+    "url": "/yulieth.com/static/js/2.81faad99.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/yulieth.com/static/js/2.81faad99.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b0848a58de121a63f288",
+    "url": "/yulieth.com/static/js/main.dbf86ad0.chunk.js"
+  },
+  {
+    "revision": "43992027b93433b3fa52",
+    "url": "/yulieth.com/static/js/runtime-main.2b402d77.js"
+  }
+]);
