59e8217ddcb99fc5506aa583027ade64ed0a1475
Yulieth Lubo <yuliethdotcom@gmail.com>
2020-03-20 20:48:25 -0500

    Improving social networks links

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import "./styles/Landing.css";
-import Github from "../src/img/github.png"
-import Linkedin from "../src/img/linkedin.png"
-import Instagram from "../src/img/instagram.png"
- import ResumeEN from "../src/pdf/resumeEN.pdf"
+import Github from "../src/img/github.png";
+import Linkedin from "../src/img/linkedin.png";
+import Instagram from "../src/img/instagram.png";
+import ResumeEN from "../src/pdf/resumeEN.pdf";
 
 class Landing extends Component {
   render() {
@@ -21,13 +21,25 @@ class Landing extends Component {
           <div className="landing__footer-container">
             <main className="landing__second">
               <a>Learn more</a>
-              <a href = {ResumeEN} target = "_blank">Just Resume</a>
+              <a href={ResumeEN} target="_blank">
+                Just Resume
+              </a>
               {/* <a href = {"/#"} target = "_blank">Just Resume</a> */}
             </main>
             <footer className="landing__footer">
-             <img src={Github} alt=""with="30" height="28"/>
-             <img src={Linkedin} alt=""with="30" height="28"/>
-             <img src={Instagram} alt=""with="30" height="28"/>
+              <a href="https://github.com/yuliethc">
+                Github
+               
+                <img src={Github} alt="Github" with="30" height="28" />
+              </a>
+              <a href="https://www.linkedin.com/in/yulieth/">
+                LinkeIn
+                <img src={Linkedin} alt="Linkedin" with="30" height="28" />
+              </a>
+              <a href="https://www.instagram.com/yuliethdotcom/">
+                Instagram
+                <img src={Instagram} alt="Instagram" with="30" height="28" />
+              </a>
             </footer>
           </div>
         </div>
