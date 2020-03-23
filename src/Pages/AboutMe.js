8e9171a5bd9662f7b50b81eb2ab6851aea55ab0a
Yulieth Lubo <yuliethdotcom@gmail.com>
2020-03-23 11:42:17 -0500

    Añadida la imagen al About me

--- src/Pages/AboutMe.js
+++ src/Pages/AboutMe.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import "./styles/AboutMe.css"
+import "./styles/AboutMe.css";
+import me from "./../src/img/me.jpg"
 
 class AboutMe extends Component {
     render() {
         return (
             <div className="about__me">
-                Get to know me
+               
+                <div className="about__me_principal">
+                    <div className="about__me_imagen-container">
+                        <img src={me} alt=""/>
+                    </div>
+                    <div>
+                    Get to know me
+                    </div>
+                </div>
             </div>
         );
     }
