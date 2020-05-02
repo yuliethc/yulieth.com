5801931d3dde19b8be06069bb220abac2535ec46
Yulieth Lubo <yuliethdotcom@gmail.com>
2020-05-02 11:57:51 -0500

    Adding projects to portfolio

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import "./styles/Portfolio.css"
+import Project from '../Components/Project';
 
 class Portfolio extends Component {
     render() {
         return (
             <div className="portfolio">
-                Portfolio
+               <span>Work</span> 
+               <section className="work__container">
+                   <Project
+                   Title="Proyecto 1"
+                   Content="blah blah blah"
+                   Web="www.yulieth.com"
+                   ></Project>
+                    <Project
+                   Title="Proyecto 2"
+                   Content="blah blah blah"
+                   Web="www.yulieth.com"
+                   ></Project>
+                    <Project
+                   Title="Proyecto 3"
+                   Content="blah blah blah"
+                   Web="www.yulieth.com"
+                   ></Project>
+                    <Project
+                   Title="Proyecto 3"
+                   Content="blah blah blah"
+                   Web="www.yulieth.com"
+                   ></Project>
+               </section>
             </div>
         );
     }
