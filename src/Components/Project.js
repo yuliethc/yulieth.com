8920fbcf5231a8b6eefef66cb54580f19ae4cc33
Yulieth Lubo <yuliethdotcom@gmail.com>
2020-05-03 09:25:10 -0500

    Added styles and props to Card of projects

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -4,9 +4,23 @@ import "./css/Project.css";
 const Project = (props) => {
   return (
     <section className="work__container-card">
-      <span> {props.Title}</span>
-      <span>{props.Content}</span>
-      <span>{props.Web}</span>
+      <div className="work__container-card-img-container">
+        <p className="work__container-card-tech-text-1">{props.TecnologiaUsada1}</p>
+        <p className="work__container-card-tech-text-2">{props.TecnologiaUsada2}</p>
+        <p className="work__container-card-tech-text-3">{props.TecnologiaUsada3}</p>
+        <p className="work__container-card-tech-text-4">{props.TecnologiaUsada4}</p>
+        <p className="work__container-card-tech-text-5">{props.TecnologiaUsada5}</p>
+        <img
+          className="work__container-card-img"
+          src={props.Image}
+          alt="Project Image"
+        />
+      </div>
+      <a className="work__container-card-web" href={props.Web}>
+      {props.TituloWeb}
+      </a>
+      <span className="work__container-card-title"> {props.Title}</span>
+      <span className="work__container-card-content">{props.Content}</span>
     </section>
   );
 };
