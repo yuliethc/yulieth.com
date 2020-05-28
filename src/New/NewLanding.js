b21f0d4cdfe4f63a02c5b0f51b7328c2df7ad3cf
yuliethc <yuliethdotcom@gmail.com>
2020-05-28 10:28:17 -0500

    Hover button Improved

--- src/New/NewLanding.js
+++ src/New/NewLanding.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Layout from './components/Layout';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Layout from "./components/Layout";
 
 class NewLanding extends Component {
-    render() {
-        return (
-           <>
-           <Layout>
-           <div
-          class="container d-flex flex-column align-items-center justify-content-end"
-          data-aos="zoom-in"
-          data-aos-delay="100"
-        >
-          <h1>Yulieth Lubo</h1>
-          <h2>Front-End Developer</h2>
-          <a href="#" class="btn-about">
-            About Me
-          </a>
-        </div>
-           </Layout>
-           </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <Layout>
+          <div
+            class="container d-flex flex-column align-items-center justify-content-end"
+            data-aos="zoom-in"
+            data-aos-delay="100"
+          >
+            <h1>Yulieth Lubo</h1>
+            <h2>Front-End Developer</h2>
+            <a href="#" class="btn-about">
+              About Me
+            </a>
+          </div>
+        </Layout>
+      </>
+    );
+  }
 }
 
 export default NewLanding;
