ac9da922788b1ec74f0062439d96fd8c725f812b
Yulieth Lubo <yuliethdotcom@gmail.com>
2020-06-21 20:43:04 -0500

    Expanding click action to the entire portfolio item container

--- src/New/components/PortfolioItem.js
+++ src/New/components/PortfolioItem.js
@@ -5,23 +5,23 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 const PortfolioItem = (props) => {
   return (
     <div
-      // class="portfolio-container"
-      // data-aos="fade-up"
-      // data-aos-delay="200"
+    // class="portfolio-container"
+    // data-aos="fade-up"
+    // data-aos-delay="200"
     >
       <div class={`col-lg-4 portfolio-item ${props.Filter}`}>
-        <div class="portfolio-wrap">
+        <div
+          class="portfolio-wrap"
+          onClick={(event, newValue) => {
+            props.openModal(props.Id);
+          }}
+        >
           <img src={props.Thumbnail} class="img-fluid" alt="Thumbnail" />
           <div class="portfolio-info">
             <h4>{props.Title}</h4>
             <p>{props.SubTitle}</p>
-            <div class="portfolio-links">
-              <a
-                onClick={(event, newValue) => {
-                  props.openModal(props.Id);
-                  
-                }}
-              >
+            <div className="portfolio-links">
+              <a>
                 <FontAwesomeIcon icon={faPlus} />
               </a>
             </div>
